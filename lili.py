8886d4dcb20f854091e71e6b8ba7bd819700a554
lilianegiebe-byte <liliane.giebe@gmail.com>
2026-01-15 19:35:19 +0100

    delete test lili

--- lili.py
+++ lili.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<p>Hello, World!</p>"
+def template():
+    return render_template("template.html")
